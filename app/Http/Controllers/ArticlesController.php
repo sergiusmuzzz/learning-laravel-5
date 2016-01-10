786cbb8a9e47c46c5702c907d1aa3e78eca89b71
sergiusmuzzz <vivaldi21>
2016-01-10 02:30:40 -0800

    Forms from

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Article;
-use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -12,12 +13,25 @@ class ArticlesController extends Controller
 {
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::latest('published_at')->get();
         return view('articles.index', compact('articles'));
     }
     public function show($id)
     {
         $article = Article::findOrFail($id);
         return view('articles.show', compact('article'));
+    }
+
+    public function create(){
+        return view('articles.create');
+    }
+
+    public function store(){
+        $input = Request::all();
+        $input['published_at'] = Carbon::now();
+
+        Article::create($input);
+
+        return  redirect('articles');
     }
 }
